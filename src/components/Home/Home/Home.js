6a3771fcb2cc11fbc68ca6f19f7936cb1597dfe7
leon-dream1~ <nahidulislam019045@gmail.com>
2024-01-29 15:10:25 +0600

    bootstrap added and project setup done

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Home.css";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div className="">
+        <Navbar/>
+      
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logo from '../../../images/logos/logo.png'
+
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+     <>
+        <div className="container">
+           <p>Hiiiiiiiiiiiiiiiiiiiii Bootstrap </p>
+        </div>
+     </>
+    );
+};
+
+export default Navbar;
